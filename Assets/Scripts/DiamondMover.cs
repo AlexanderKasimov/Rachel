cf598a0754ba285bb697ae176cfa7f8d46237aec
AlexanderKasimov <strelokstalk@yandex.ru>
2019-05-12 13:32:59 +0500

    Update 4
    -Farm_04
    -Farm_05
    -ArtifactActivation

--- Assets/Scripts/DiamondMover.cs
+++ Assets/Scripts/DiamondMover.cs
@@ -6,14 +6,21 @@ public class DiamondMover : MonoBehaviour
 {
     Vector2 startPosition;
     Vector2 moveVector = new Vector2(0f, 1f);
+    bool isFirstTime = true;
+    float speed;
 
-    public float speed = 2f;
-    public GameObject endPlace;    
+    public float startSpeed = 2f;
+    public float endSpeed = 1f;
+    public GameObject startPlace;
+    public GameObject endPlace;
+
+
 
 
     private void Awake()
     {
         startPosition = transform.position;
+        speed = startSpeed;
     }
 
     // Start is called before the first frame update
@@ -27,12 +34,16 @@ public class DiamondMover : MonoBehaviour
     {
         if (Vector2.Distance(transform.position,endPlace.transform.position) < 0.05f )
         {
-            moveVector = new Vector2(0f, -1f);
+            moveVector = (startPlace.transform.position - endPlace.transform.position).normalized;
+            isFirstTime = false;
+            speed = endSpeed;            
         }
-        if (Vector2.Distance(transform.position, startPosition) < 0.05f)   
+        if (Vector2.Distance(transform.position, startPlace.transform.position) < 0.05f && !isFirstTime)   
         {
-            moveVector = new Vector2(0f, 1f);
-        }
+            moveVector = -moveVector;
+        }   
+            
+           
 
         transform.Translate(moveVector*speed*Time.deltaTime);
     }
